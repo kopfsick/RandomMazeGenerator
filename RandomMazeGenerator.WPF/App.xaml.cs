057a3bbd50cca725e5b50519c2c75464135f4cec
Julian Hanssen <julian@kopfsick.com>
2020-05-05 12:33:59 +0200

    Hooked up simple MainViewModel

--- RandomMazeGenerator.WPF/App.xaml.cs
+++ RandomMazeGenerator.WPF/App.xaml.cs
@@ -13,5 +13,11 @@ namespace RandomMazeGenerator.WPF
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var window = new MainWindow(){DataContext = new MainViewModel()};
+            window.Show();
+        }
     }
 }
---
+++ RandomMazeGenerator.WPF/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace RandomMazeGenerator.WPF
+{
+    public class MainViewModel : NotifyPropertyChangedBase
+    {
+    }
+}
