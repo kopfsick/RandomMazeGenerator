784da8e0dc1db2c8d27afe4ef50c8325789b0085
Julian Hanssen <julian@kopfsick.com>
2020-05-22 14:06:11 +0200

    Added basic settings to Skia.WPF application. Simplified Skia Paints in Skia.WPF application. Added simplex noise to wall colors in Skia.WPF application

--- RandomMazeGenerator.Skia.WPF/MainWindow.xaml.cs
+++ RandomMazeGenerator.Skia.WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using RandomMazeGenerator.WPF;
+﻿using PropertyTools.DataAnnotations;
+using RandomMazeGenerator.WPF;
 using SharpNoise.Modules;
 using SkiaSharp;
 using System;
@@ -19,6 +20,19 @@ using System.Windows.Shapes;
 
 namespace RandomMazeGenerator.Skia.WPF
 {
+    public class Settings
+    {
+        [Slidable(1,100, 1, 10, true, 10)]
+        [HeaderPlacement(HeaderPlacement.Above)]
+        public int StepsPerUpdate { get; set; } = 10;
+        
+        [Slidable(1,100, 1, 10, true, 10)]
+        [HeaderPlacement(HeaderPlacement.Above)]
+        public int StepsPerUpdateSolving { get; set; } = 10;
+
+        public bool VizualizeStack { get; set; } = false;
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -29,12 +43,10 @@ namespace RandomMazeGenerator.Skia.WPF
         private AStarPathFindingAlgorithm _solvingAlgorithm;
         private float _cellWidth;
         private float _cellHeight;
-        private int _stepsPerUpdate = 1;
-        private int _stepsPerUpdateSolving;
-        private bool _visualizeStack = false;
         private Module _noise = new Simplex();
         private double _noiseOffset = 0;
         private SKPaint _bestCellPaint;
+        private SKPaint _onStackPaint;
         private SKPaint _wallPaint;
         private SKPaint _currentCellPaint;
 
@@ -42,33 +54,49 @@ namespace RandomMazeGenerator.Skia.WPF
         {
             InitializeComponent();
 
-            _visualizeStack = false;
-            _stepsPerUpdate = 30;
-            _stepsPerUpdateSolving = 10;
+            Settings = new Settings()
+            {
+                StepsPerUpdate = 10,
+                StepsPerUpdateSolving = 10
+                };
+
+            PropertyGrid.SelectedObject = Settings;
+
             var mazeWidth = 100;
             _maze = new Maze(mazeWidth);
             _cellWidth = 2f/mazeWidth;
 
             _solvingAlgorithm = new AStarPathFindingAlgorithm(_maze);
             _algorithm = new DepthFirstRecursiveBacktrackingMazeAlgorithm(_maze);
-            _algorithm = new RandomizedPrimsMazeAlgorithm(_maze);
+            //_algorithm = new RandomizedPrimsMazeAlgorithm(_maze);
 
 
             _bestCellPaint = CreateFillPaint(SKColors.Yellow);
+            _onStackPaint = CreateFillPaint(SKColors.Blue);
             _wallPaint = CreateStrokePaint(SKColors.Blue);
             _currentCellPaint = CreateFillPaint(SKColors.LightBlue);
 
             DoGameLoopAsync();
         }
 
+        public Settings Settings { get; set; }
+
         private async Task DoGameLoopAsync()
         {
             while(true)
             {
                 await Task.Run(() => Update());
+                _noiseOffset += 0.01;
                 SkiaElement.InvalidateVisual();
                 await Task.Delay(16);
             }
+        }
+
+        private void SkiaControl_PaintSurface(object sender, SkiaSharp.Views.Desktop.SKPaintSurfaceEventArgs e)
+        {
+            SKCanvas canvas = e.Surface.Canvas;
+
+            Draw(e, canvas);
         }
 
         private void SkiaElement_PaintSurface(object sender, SkiaSharp.Views.Desktop.SKPaintSurfaceEventArgs e)
@@ -87,6 +115,10 @@ namespace RandomMazeGenerator.Skia.WPF
 
             foreach(var cell in _maze.Cells)
             {
+                var v = (float)((_noise.GetValue(cell.X/5d, cell.Y/5d, _noiseOffset)+1) *0.5);
+                float v1 = (127 + (v * 128));
+                _wallPaint.Color = new SKColor(0,0, (byte)Math.Clamp(v1, 0, 255));
+
                 var topLeftX = cell.X * _cellWidth - 1;
                 var topLeftY = cell.Y * _cellHeight - 1;
 
@@ -95,9 +127,15 @@ namespace RandomMazeGenerator.Skia.WPF
                     canvas.DrawRect(topLeftX, topLeftY, _cellWidth, _cellHeight, _currentCellPaint);
                 }
 
-                if(cell.IsOnStack && _visualizeStack)
+                if(cell.IsOnStack && Settings.VizualizeStack)
                 {
-                    //Visualize on stack
+                    float heightOffset = _cellHeight / 10;
+                    float widthOffset = _cellWidth / 10;
+
+                    heightOffset = 0;
+                    widthOffset = 0;
+
+                    canvas.DrawRect(topLeftX + widthOffset, topLeftY + heightOffset, _cellWidth - widthOffset, _cellHeight - heightOffset, _onStackPaint);
                 }
 
                 if(cell.HasBeenVisited)
@@ -133,21 +171,23 @@ namespace RandomMazeGenerator.Skia.WPF
 
         private static SKPaint CreateStrokePaint(SKColor color)
         {
-            return new SKPaint() { Color = SKColors.HotPink, IsStroke = true, StrokeWidth = 1, Shader = SKShader.CreateColor(color) };
+            return new SKPaint() { Color = color, Style = SKPaintStyle.Stroke };
         }
 
         private static SKPaint CreateFillPaint(SKColor color)
         {
-            return new SKPaint() { Color = SKColors.HotPink, IsStroke = false, StrokeWidth = 1, Shader = SKShader.CreateColor(color) };
+            return new SKPaint() { Color = color, Style = SKPaintStyle.StrokeAndFill };
         }
 
         private void Update()
         {
             if(!_algorithm.IsFinished)
-                _algorithm.Step(_stepsPerUpdate);
+                _algorithm.Step(Settings.StepsPerUpdate);
             else if(!_solvingAlgorithm.IsFinished)
-                _solvingAlgorithm.Step(_stepsPerUpdateSolving);
+                _solvingAlgorithm.Step(Settings.StepsPerUpdateSolving);
             _noiseOffset += 0.01;
         }
+
+        
     }
 }
